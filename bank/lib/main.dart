ba8f2a88225e64cdb88774d847ae9456757d0db0
Victor Dalacôrte <victor.dalacorte@ipcorp.com.br>
2021-06-23 20:20:59 -0300

    flutter

--- bank/lib/main.dart
+++ bank/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Text(
-    "Hello, world!",
-    textDirection: TextDirection.ltr,
-  ));
+  runApp(
+    Text(
+      "Hello, world!",
+      textDirection: TextDirection.ltr,
+    ),
+  );
 }
